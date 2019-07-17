a1b81038377891c69e60583a7a24db5dc185aac5
Laughing Zhang <lin.zhang@advantest.com>
2019-07-17 13:58:06 +0800

    add chapter 3 code

---
+++ practice/com.advantest.threadLearn/src/com/advantest/chapter3/notifyAll/src/service/Service.java
@@ -0,0 +1,21 @@
+package service;
+
+public class Service {
+
+	public void testMethod(Object lock) {
+		try {
+			synchronized (lock) {
+				System.out.println("begin wait() ThreadName="
+						+ Thread.currentThread().getName());
+				Thread.sleep(1000);
+				lock.wait();
+				Thread.sleep(1000);
+				System.out.println("  end wait() ThreadName="
+						+ Thread.currentThread().getName());
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ practice/com.advantest.threadLearn/src/com/advantest/chapter3/waitReleaseLock/src/Java/testMethod.java
@@ -0,0 +1,16 @@
+package Java;
+
+import java.util.Iterator;
+
+public class testMethod {
+
+}
+
+
+class TestIterable<String> implements Iterable{
+
+	public Iterator iterator() {
+		return null;
+	}
+	
+}
